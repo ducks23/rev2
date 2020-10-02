0174bc881aef95481966de730af2be5e7cf17a9e
jesse <jesse1517@gmail.com>
2020-10-02 23:27:59 +0000

    updated scripts

--- scripts/deploy-focal.sh
+++ scripts/deploy-focal.sh
@@ -4,6 +4,6 @@ set -e
 
 stage=$1
 
-for charm in slurmctld slurmd slurmdbd slurmconfigurator; do
+for charm in slurmctld slurmd slurmdbd slurm-configurator; do
     juju deploy ./$charm.charm --resource slurm=./slurm.resource --series focal
 done
