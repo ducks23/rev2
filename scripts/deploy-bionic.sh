795e24da0483b483f1e2090d76ed196c057c1f82
jesse <jesse1517@gmail.com>
2020-10-05 22:52:17 +0000

    bind nat

--- scripts/deploy-bionic.sh
+++ scripts/deploy-bionic.sh
@@ -5,5 +5,5 @@ set -e
 stage=$1
 
 for charm in slurmctld slurmd slurmdbd slurm-configurator; do
-	juju deploy ./$charm.charm --resource slurm=./slurm.resource --series bionic
+	juju deploy ./$charm.charm --resource slurm=./slurm.resource --series bionic --bind nat
 done
