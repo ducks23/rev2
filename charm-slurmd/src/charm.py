8dca7383dcd3955702fd219b6d572013f871f8d0
jesse <jesse1517@gmail.com>
2020-10-05 22:15:05 +0000

    fixed slurmd

--- charm-slurmd/src/charm.py
+++ charm-slurmd/src/charm.py
@@ -54,9 +54,6 @@ class SlurmdCharm(CharmBase):
 
             self._slurmd.on.slurm_config_available:
             self._on_check_status_and_write_config,
-
-            self.on.set_node_state_action:
-            self._on_node_state_action,
         }
         for event, handler in event_handler_bindings.items():
             self.framework.observe(event, handler)
