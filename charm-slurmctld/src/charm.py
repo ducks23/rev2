d2a9252d607231a5cb143d2fdc363c942c6fa8b6
jesse <jesse1517@gmail.com>
2020-10-07 23:33:49 +0000

    linted

--- charm-slurmctld/src/charm.py
+++ charm-slurmctld/src/charm.py
@@ -130,8 +130,9 @@ class SlurmctldCharm(CharmBase):
     def get_port(self):
         """Return the port."""
         return self._slurm_manager.port
-    
-    def set_slurm_configurator_available(boolean):
+
+    def set_slurm_configurator_available(self, boolean):
+        """Set configurator status."""
         self._stored.slurm_configurator_available = boolean
 
 
