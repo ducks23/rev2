3f170ad45066e36f711b7332405fd3d286c35635
jesse <jesse1517@gmail.com>
2020-10-09 17:59:51 +0000

    fix

--- charm-slurmrestd/src/slurmrestd_requires.py
+++ charm-slurmrestd/src/slurmrestd_requires.py
@@ -39,6 +39,7 @@ class SlurmrestdRequires(Object):
         """Set the provides initial data."""
         super().__init__(charm, relation_name)
         self.charm = charm
+        self._relation_name = relation_name
 
         self.framework.observe(
             charm.on[relation_name].relation_changed,
@@ -55,7 +56,10 @@ class SlurmrestdRequires(Object):
         if not event_app_data:
             event.defer()
             return
-        # Store the munge_key in the charm's state
+        slurm_config = event_app_data.get('slurm_config')
+        if not slurm_config:
+            event.defer()
+            return
         self.charm.set_config_available(True)
         self.on.config_available.emit()
 
